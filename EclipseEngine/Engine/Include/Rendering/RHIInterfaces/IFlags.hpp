968d4a6bf995ec781b375e36fc4ec689827d1f71
Yanis SCHWARZ <y.schwarz@student.isartdigital.com>
2025-03-20 14:43:10 +0100

    RHI
    [Updated] enum to enum class
    [Added] Last element added to enum class to have size

--- EclipseEngine/Engine/Include/Rendering/RHIInterfaces/IFlags.hpp
+++ EclipseEngine/Engine/Include/Rendering/RHIInterfaces/IFlags.hpp
@@ -4,26 +4,27 @@
 
 namespace RHI
 {
-	enum IFLAGS
-	{
-		TYPE_FLOAT = 1,
-        COLOR_BUFFER_BIT = 2,
-        DEPTH_BUFFER_BIT = 3,
-        STENCIL_BUFFER_BIT = 4,
-        DEPTH_LESS = 5,
-        DEPTH_LEQUAL = 6
+    enum class IFLAGS : int
+    {
+        TYPE_FLOAT = 0,
+        COLOR_BUFFER_BIT = 1,
+        DEPTH_BUFFER_BIT = 2,
+        STENCIL_BUFFER_BIT = 3,
+        DEPTH_LESS = 4,
+        DEPTH_LEQUAL = 5,
+        COUNT
 	};
 
     inline int CastToOpenGLFlags(IFLAGS _type)
     {
-        int typeIndex = (int)_type;
-        static const std::array<int, 6> openGLMacro =
+        int typeIndex = static_cast<int>(_type);
+        static const std::array<int, static_cast<int>(IFLAGS::COUNT)> openGLMacro =
         {
             GL_FLOAT,           // TYPE
             GL_COLOR_BUFFER_BIT, GL_DEPTH_BUFFER_BIT, GL_STENCIL_BUFFER_BIT,        // BUFFER_BIT
             GL_LESS, GL_LEQUAL      // DEPTH_COMPARISON
         };
 
-        return openGLMacro[typeIndex - 1];
+        return openGLMacro[typeIndex];
     }
 }
