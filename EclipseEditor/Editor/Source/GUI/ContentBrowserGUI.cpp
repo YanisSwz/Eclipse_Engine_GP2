7eb4ecc02d9b7fa1a896f1d44d663f30d973b0f6
Lucas Lepinay <l.lepinay@student.isartdigital.com>
2025-04-12 15:34:26 +0200

    Content Browser / Inspector
    [Added] Drag&Drop for texture from content browser to inspector

--- EclipseEditor/Editor/Source/GUI/ContentBrowserGUI.cpp
+++ EclipseEditor/Editor/Source/GUI/ContentBrowserGUI.cpp
@@ -18,22 +18,14 @@ namespace GUI
 
 	void ContentBrowseGUI::Draw()
 	{
-		//std::vector<std::string> meshNames = Resource::ResourceManager::GetInstance().GetAllResourceWithType<Resource::Mesh>();
-		std::vector<std::string> textureNames = Resource::ResourceManager::GetInstance().GetAllResourceWithType<Resource::Texture>();
-		//std::vector<std::string> vertShaderNames = Resource::ResourceManager::GetInstance().GetAllResourceWithType<Resource::VertShader>();
-		//std::vector<std::string> fragShaderNames = Resource::ResourceManager::GetInstance().GetAllResourceWithType<Resource::FragShader>();
-		//std::vector<std::string> shaderProgramNames = Resource::ResourceManager::GetInstance().GetAllResourceWithType<Resource::ShaderProgram>();
-		//std::vector<std::string> skyboxNames = Resource::ResourceManager::GetInstance().GetAllResourceWithType<Resource::Skybox>();
+		static std::vector<std::string> textureNames = Resource::ResourceManager::GetInstance().GetAllResourceWithType<Resource::Texture>();
 
 		ImGuiWindowFlags contentBrowserWindowFlags = ImGuiWindowFlags_None;
-
 		ImGui::Begin("Content Browser", 0, contentBrowserWindowFlags);
 
 		Resource::Texture* texture;
-
 		int nbElem = static_cast<int>(textureNames.size());
 		int nbElemInCollumn = static_cast<int>(ImGui::GetContentRegionAvail().x / 100.f) - 1;
-
 		if (nbElemInCollumn > 0)
 		{
 			if (ImGui::BeginTable("Content Browser Table", nbElemInCollumn, ImGuiTableFlags_NoBordersInBody))
@@ -43,13 +35,28 @@ namespace GUI
 					if (i % nbElemInCollumn == 0)
 						ImGui::TableNextRow();
 
+					ImGui::PushID(i);
 					ImGui::TableSetColumnIndex(i % nbElemInCollumn);
 					texture = Resource::ResourceManager::GetInstance().GetResource<Resource::Texture>(textureNames[i]);
 					DrawImage(textureNames[i].c_str(), texture->GetID(), 100.f);
+
+					if (ImGui::BeginDragDropSource(ImGuiDragDropFlags_None))
+					{
+						ImGui::SetDragDropPayload("TextureName", &textureNames[i], sizeof(std::string));
+						DrawImage(textureNames[i].c_str(), texture->GetID(), 100.f);
+						ImGui::Text(textureNames[i].c_str());
+						ImGui::EndDragDropSource();
+					}
+					ImGui::PopID();
+
+
+					ImGui::Text(textureNames[i].c_str());
 				}
 				ImGui::EndTable();
 			}
 		}
+
+
 
 		ImGui::End();
 	}
--- EclipseEditor/Editor/Source/GUI/InspectorGUI.cpp
+++ EclipseEditor/Editor/Source/GUI/InspectorGUI.cpp
@@ -1,6 +1,8 @@
 #include "GUI/InspectorGUI.hpp"
 #include "GUI/Widget/ImGuiWidget.hpp"
 #include "GameObject.hpp"
+#include "Resource/ResourceManager.hpp"
+#include "Resource/Texture.hpp"
 
 namespace GUI
 {
@@ -15,14 +17,56 @@ namespace GUI
 	void InspectorGUI::Draw(Core::GameObject* _crtGOselected)
 	{
 		ImGuiWindowFlags inspectorWindowFlags = ImGuiWindowFlags_None;
-
 		ImGui::Begin("Inspector", 0, inspectorWindowFlags);
 
+		ImGui::SeparatorText("Transform");
 		if (_crtGOselected)
 		{
 			GUI::DragVec3XYZ("Position", _crtGOselected->transform->localPosition);
 			GUI::DragQuatXYZ("Rotation", _crtGOselected->transform->localRotation);
 			GUI::DragVec3XYZ("Scale", _crtGOselected->transform->localScale, 1.f);
+		}
+
+		ImGui::SeparatorText("Model");
+		static std::vector<std::string> textureNames = Resource::ResourceManager::GetInstance().GetAllResourceWithType<Resource::Texture>();
+		static std::string crtTexture = "";
+
+		ImGui::Columns(2);
+		ImGui::SetColumnWidth(0, 100.f);
+		ImGui::Text("Texture: ");
+		ImGui::NextColumn();
+		static ImGuiComboFlags comboFlags = 0;
+		if (ImGui::BeginCombo("##Texture Combo (w/ filter)", crtTexture.c_str(), comboFlags))
+		{
+			static ImGuiTextFilter filter;
+			if (ImGui::IsWindowAppearing())
+			{
+				ImGui::SetKeyboardFocusHere();
+				filter.Clear();
+			}
+			ImGui::SetNextItemShortcut(ImGuiMod_Ctrl | ImGuiKey_F);
+			filter.Draw("##Filter", -FLT_MIN);
+
+			for (int i = 0; i < textureNames.size(); ++i)
+			{
+				const bool is_selected = (crtTexture == textureNames[i]);
+				if (filter.PassFilter(textureNames[i].c_str()))
+					if (ImGui::Selectable(textureNames[i].c_str(), is_selected))
+						crtTexture = textureNames[i];
+			}
+			ImGui::EndCombo();
+		}
+
+		if (ImGui::BeginDragDropTarget())
+		{
+			if (const ImGuiPayload* payload = ImGui::AcceptDragDropPayload("TextureName"))
+			{
+				IM_ASSERT(payload->DataSize == sizeof(std::string));
+				std::string payload_n;
+				payload_n = *static_cast<std::string*>(payload->Data);
+				crtTexture = payload_n;
+			}
+			ImGui::EndDragDropTarget();
 		}
 
 		ImGui::End();
