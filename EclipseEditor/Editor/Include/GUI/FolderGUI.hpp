0c283271efe9f7b343f932cceed3979484f8fe4b
Lucas Lepinay <l.lepinay@student.isartdigital.com>
2025-04-13 12:42:17 +0200

    Content Browser
    [Added] Mesh files in folder with Drag&Drop

--- EclipseEditor/Editor/Include/GUI/FolderGUI.hpp
+++ EclipseEditor/Editor/Include/GUI/FolderGUI.hpp
@@ -5,6 +5,7 @@
 namespace Resource
 {
 	class Texture;
+	class Mesh;
 }
 
 namespace GUI
@@ -21,12 +22,15 @@ namespace GUI
 		FolderGUI* Draw();
 		FolderGUI* DrawFolderGUI(int _index);
 		void DrawTextureGUI(int _index);
+		void DrawMeshGUI(int _index);
 		void Delete();
 
 	private:
 		std::vector<FolderGUI*> m_folderChildren;
 		std::vector<Resource::Texture*> m_textureFiles;
+		std::vector<Resource::Mesh*> m_meshFiles;
 
 		static Resource::Texture* m_folderIcon;
+		static Resource::Texture* m_meshIcon;
 	};
 }
--- EclipseEditor/Editor/Source/GUI/FolderGUI.cpp
+++ EclipseEditor/Editor/Source/GUI/FolderGUI.cpp
@@ -1,11 +1,13 @@
 #include "GUI/FolderGUI.hpp"
 #include "Resource/ResourceManager.hpp"
 #include "Resource/Texture.hpp"
+#include "Resource/Mesh.hpp"
 #include "GUI/Widget/ImGuiWidget.hpp"
 
 namespace GUI
 {
 	Resource::Texture* FolderGUI::m_folderIcon = nullptr;
+	Resource::Texture* FolderGUI::m_meshIcon = nullptr;
 
 	FolderGUI::FolderGUI(std::string _name)
 		: name(_name)
@@ -26,6 +28,10 @@ namespace GUI
 			for (std::string textureName : textureNames)
 				m_textureFiles.push_back(Resource::ResourceManager::GetInstance().GetResource<Resource::Texture>(textureName));
 
+			std::vector<std::string> meshNames = Resource::ResourceManager::GetInstance().GetAllResourceWithType<Resource::Mesh>();
+			for (std::string meshName : meshNames)
+				m_meshFiles.push_back(Resource::ResourceManager::GetInstance().GetResource<Resource::Mesh>(meshName));
+
 			FolderGUI* textureFolder = new FolderGUI("Editor");
 			m_folderChildren.push_back(textureFolder);
 
@@ -33,6 +39,7 @@ namespace GUI
 		}
 
 		m_folderIcon = Resource::ResourceManager::GetInstance().GetResource<Resource::Texture>("FolderIcon.img");
+		m_meshIcon = m_folderIcon;
 		FolderGUI* textureFolder = new FolderGUI("Texture");
 		m_folderChildren.push_back(textureFolder);
 		textureFolder->Init();
@@ -43,8 +50,9 @@ namespace GUI
 
 		int folderChildrenSize = static_cast<int>(m_folderChildren.size());
 		int texturesFilesSize = static_cast<int>(m_textureFiles.size());
+		int meshFilesSize = static_cast<int>(m_meshFiles.size());
 
-		int nbElem = folderChildrenSize + texturesFilesSize;
+		int nbElem = folderChildrenSize + texturesFilesSize + meshFilesSize;
 		int nbElemInCollumn = static_cast<int>(ImGui::GetContentRegionAvail().x / 100.f) - 1;
 		if (nbElemInCollumn > 0)
 		{
@@ -65,6 +73,10 @@ namespace GUI
 					else if (i < folderChildrenSize + texturesFilesSize)
 					{
 						DrawTextureGUI(i);
+					}
+					else if (i < folderChildrenSize + texturesFilesSize + meshFilesSize)
+					{
+						DrawMeshGUI(i);
 					}
 				}
 				ImGui::EndTable();
@@ -104,6 +116,23 @@ namespace GUI
 			ImGui::EndDragDropSource();
 		}
 		ImGui::Text(texture->name.c_str());
+		ImGui::PopID();
+	}
+
+	void FolderGUI::DrawMeshGUI(int _index)
+	{
+		ImGui::PushID(_index);
+		Resource::Mesh* mesh = m_meshFiles[_index - m_folderChildren.size() - m_textureFiles.size()];
+		DrawImage(mesh->name.c_str(), m_meshIcon->GetID(), 100.f);
+
+		if (ImGui::BeginDragDropSource(ImGuiDragDropFlags_None))
+		{
+			ImGui::SetDragDropPayload("MeshName", &mesh->name, sizeof(std::string));
+			DrawImage(mesh->name.c_str(), m_meshIcon->GetID(), 100.f);
+			ImGui::Text(mesh->name.c_str());
+			ImGui::EndDragDropSource();
+		}
+		ImGui::Text(mesh->name.c_str());
 		ImGui::PopID();
 	}
 
