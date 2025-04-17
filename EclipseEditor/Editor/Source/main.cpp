ec13190bd30b8c937bd0d9e89f5705af10657d1b
Arthur GUEDU <a.guedu@student.isartdigital.com>
2025-04-17 13:51:17 +0200

    Editor
    [Added] Standard Windows Console not launched with Editor

--- EclipseEditor/Editor/Source/main.cpp
+++ EclipseEditor/Editor/Source/main.cpp
@@ -1,19 +1,26 @@
-#include "imgui.h"
+#define NOGDI
+#include <windows.h>
+#undef CreateWindow
 
 #include "EditorApp.hpp"
 #ifndef NDEBUG
 #include <crtdbg.h>
 #endif
 
-int main()
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nCmdShow)
 {
+	(void)(hInstance);
+	(void)(hPrevInstance);
+	(void)(lpCmdLine);
+	(void)(nCmdShow);
+
 	/// Used for memory leak debug.
 	#ifndef NDEBUG
 		_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 		//_CrtSetBreakAlloc(531);
 	#endif
 
-	EditorApp* editorApp = new EditorApp{"Eclipse Engine", 1280, 720};
+	EditorApp* editorApp = new EditorApp{ "Eclipse Engine", 1280, 720 };
 
 	while (!editorApp->ShouldClose())
 	{
