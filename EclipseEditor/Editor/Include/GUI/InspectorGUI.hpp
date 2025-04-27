709d2ecf868c98977c83c8ab3b3e8f57a6eb9da5
Lucas Lepinay <l.lepinay@student.isartdigital.com>
2025-04-27 23:19:54 +0200

    Inspector
    [Added] Add Component Button in Inspector

--- EclipseEditor/Editor/Include/GUI/InspectorGUI.hpp
+++ EclipseEditor/Editor/Include/GUI/InspectorGUI.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <imgui.h>
 
 namespace Core
 {
@@ -8,6 +9,9 @@ namespace Core
 	class BoxCollider;
 	class CapsuleCollider;
 	class MeshCollider;
+	class DirectionalLight;
+	class PointLight;
+	class SpotLight;
 }
 
 namespace GUI
@@ -15,10 +19,10 @@ namespace GUI
 	class InspectorGUI
 	{
 	public:
-		InspectorGUI() = default;
+		InspectorGUI();
 		~InspectorGUI() = default;
 
-		void Draw(Core::GameObject* _crtGOselected);
+		void Draw(Core::GameObject* _crtGOSelected);
 
 	private:
 		void DrawTransformComponent(Core::Transform* _transform);
@@ -26,6 +30,21 @@ namespace GUI
 		void DrawBoxColliderComponent(Core::BoxCollider* _collider);
 		void DrawCapsuleColliderComponent(Core::CapsuleCollider* _collider);
 		void DrawMeshColliderComponent(Core::MeshCollider* _collider);
+		void DrawDirectionalLightComponent(Core::DirectionalLight* _light);
+		void DrawPointLightComponent(Core::PointLight* _light);
+		void DrawSpotLightComponent(Core::SpotLight* _light);
 
+		void DrawAddComponent(Core::GameObject* _crtGOSelected);
+		void DrawAddRendererComponent(Core::GameObject* _crtGOSelected);
+		void DrawAddColliderComponent(Core::GameObject* _crtGOSelected);
+		void DrawAddLightComponent(Core::GameObject* _crtGOSelected);
+
+		ImGuiTreeNodeFlags m_treeNodeComponentFlags = ImGuiTreeNodeFlags_None;
+		ImGuiTreeNodeFlags m_treeNodeAddComponentFlags = ImGuiTreeNodeFlags_None;
+		ImGuiPopupFlags m_PopupAddComponentFlags = ImGuiPopupFlags_None;
+		
+		ImGuiWindowFlags m_alreadyAddComponentWindowFlags = ImGuiWindowFlags_None;
+		ImVec2 m_alreadyAddComponentWindowSize{ 300.f, 400.f };
+		bool bisComponentAlreadyAddedWindowEnable = false;
 	};
 }
