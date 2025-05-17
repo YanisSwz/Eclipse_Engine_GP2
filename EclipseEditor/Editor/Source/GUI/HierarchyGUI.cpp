ca3105c31bad4b6b55d3bb4e98d07c46d25e9f3b
Arthur GUEDU <a.guedu@student.isartdigital.com>
2025-05-17 11:24:41 +0200

    Editor
    [Added] Prefab DragAndDrop on Scene in Hierarchy

--- EclipseEditor/Editor/Source/GUI/HierarchyGUI.cpp
+++ EclipseEditor/Editor/Source/GUI/HierarchyGUI.cpp
@@ -41,6 +41,17 @@ namespace GUI
 					transform->SetRotation(transform->GetRotation());
 					transform->SetScale(transform->GetScale());
 				}
+
+				if (const ImGuiPayload* payload = ImGui::AcceptDragDropPayload("PrefabName"))
+				{
+					IM_ASSERT(payload->DataSize == sizeof(std::string));
+					m_crtPrefabName = *static_cast<std::string*>(payload->Data);
+
+					m_crtPrefab = Resource::ResourceManager::GetInstance().GetResource<Resource::Prefab>(m_crtPrefabName);
+					_scene->InstantiatePrefab(nullptr, m_crtPrefab);
+					m_crtPrefabName.clear();
+					m_crtPrefab = nullptr;
+				}
 				ImGui::EndDragDropTarget();
 			}
 			
@@ -102,6 +113,18 @@ namespace GUI
 					transform->SetRotation(transform->GetRotation());
 					transform->SetScale(transform->GetScale());
 				}
+
+				if (const ImGuiPayload* payload = ImGui::AcceptDragDropPayload("PrefabName"))
+				{
+					IM_ASSERT(payload->DataSize == sizeof(std::string));
+					m_crtPrefabName = *static_cast<std::string*>(payload->Data);
+
+					m_crtPrefab = Resource::ResourceManager::GetInstance().GetResource<Resource::Prefab>(m_crtPrefabName);
+					_scene->InstantiatePrefab(_crtTransform->GetGameObject(), m_crtPrefab);
+					m_crtPrefabName.clear();
+					m_crtPrefab = nullptr;
+				}
+
 				ImGui::EndDragDropTarget();
 			}
 
@@ -195,21 +218,6 @@ namespace GUI
 				Core::GameObject* tempNewGOSelected = RecursiveDraw(transform, _scene, _crtGOSelected);
 				if (tempNewGOSelected)
 					newGameObjectSelected = tempNewGOSelected;
-			}
-
-			if (ImGui::BeginDragDropTarget())
-			{
-				if (const ImGuiPayload* payload = ImGui::AcceptDragDropPayload("PrefabName"))
-				{
-					IM_ASSERT(payload->DataSize == sizeof(std::string));
-					m_crtPrefabName = *static_cast<std::string*>(payload->Data);
-
-					m_crtPrefab = Resource::ResourceManager::GetInstance().GetResource<Resource::Prefab>(m_crtPrefabName);
-					_scene->InstantiatePrefab(_crtTransform->GetGameObject(), m_crtPrefab);
-					m_crtPrefabName.clear();
-					m_crtPrefab = nullptr;
-				}
-				ImGui::EndDragDropTarget();
 			}
 
 			ImGui::TreePop();
